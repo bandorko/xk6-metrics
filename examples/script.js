b1bd75e9492927ec286205746f9ed76a6d0d53fc
Balazs Andorko <balazs.andorko@gmail.com>
2023-11-25 07:42:52 +0100

    register metrics automatically

--- examples/script.js
+++ examples/script.js
@@ -1,18 +1,3 @@
-# xk6-metrics
-[k6](https://github.com/grafana/k6) extension for accessing various derived metrics.
-
-Currently supported metrics:
-- drop_percentage
-
-## Build
-you can build the custom k6 binary with [xk6](https://github.com/grafana/xk6)
-
-```shell
-xk6 build --with github.com/bandorko/xk6-metrics@latest
-```
-
-## Example
-```javascript
 import { sleep } from 'k6';
 import { Gauge } from 'k6/metrics';
 import metrics from  'k6/x/metrics';
@@ -37,9 +22,3 @@ export default function(){
     sleep(0.5);  // 2 iterations/second/VU
 }
 
-```
-
-## Run sample script
-```shell
-./k6 run examples/script.js
-```
--- metrics.go
+++ metrics.go
@@ -1,6 +1,10 @@
 package metrics
 
 import (
+	"sync"
+	"time"
+
+	"go.k6.io/k6/event"
 	"go.k6.io/k6/js/modules"
 	"go.k6.io/k6/metrics"
 )
@@ -9,8 +13,14 @@ func init() {
 	modules.Register("k6/x/metrics", New())
 }
 
+type extraMetrics struct {
+	DropPercentage *metrics.Metric
+}
+
 type (
-	RootModule struct{}
+	RootModule struct {
+		initOnce sync.Once
+	}
 
 	ModuleInstance struct {
 		vu modules.VU
@@ -23,10 +33,49 @@ var (
 )
 
 func New() *RootModule {
-	return &RootModule{}
+	return &RootModule{
+		initOnce: sync.Once{},
+	}
 }
 
-func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+func (rm *RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	rm.initOnce.Do(
+		func() {
+			registry := vu.InitEnv().Registry
+			m := &extraMetrics{
+				DropPercentage: registry.MustNewMetric("drop_percentage", metrics.Counter),
+			}
+			sid, evtCh := vu.Events().Global.Subscribe(event.TestEnd)
+			go func() {
+				for {
+					select {
+					case evt, ok := <-evtCh:
+						if !ok {
+							return
+						}
+						evt.Done()
+						if evt.Type == event.TestEnd {
+							droppedIterations := registry.Get("dropped_iterations").Sink.(*metrics.CounterSink).Value
+							iterations := registry.Get("iterations").Sink.(*metrics.CounterSink).Value
+							dropPercentage := droppedIterations / (iterations + droppedIterations)
+							m.DropPercentage.Sink.Add(
+								metrics.Sample{
+									Time: time.Now(),
+									TimeSeries: metrics.TimeSeries{
+										Metric: m.DropPercentage,
+										Tags:   &metrics.TagSet{},
+									},
+									Value: float64(dropPercentage),
+								})
+							vu.Events().Global.Unsubscribe(sid)
+						}
+					case <-vu.Context().Done():
+						return
+					}
+				}
+			}()
+		})
+
 	return &ModuleInstance{
 		vu: vu,
 	}
@@ -34,12 +83,6 @@ func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
 
 func (mi *ModuleInstance) Exports() modules.Exports {
 	return modules.Exports{
-		Named: map[string]interface{}{
-			"getDropPercentage": func() interface{} {
-				droppedIterations := mi.vu.State().BuiltinMetrics.DroppedIterations.Sink.(*metrics.CounterSink).Value
-				iterations := mi.vu.State().BuiltinMetrics.Iterations.Sink.(*metrics.CounterSink).Value
-				return droppedIterations / (iterations + droppedIterations)
-			},
-		},
+		Named: make(map[string]interface{}),
 	}
 }
